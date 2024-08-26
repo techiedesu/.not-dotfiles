94ddf37a753723ba0705df19c776729e8fb049e5
Vlad Dyshakov <kawaii.nekotech@gmail.com>
2024-08-26 12:32:32 +0700

    Updated PS1: $ -> %

--- not-imports/not-promt.zsh
+++ not-imports/not-promt.zsh
@@ -15,7 +15,7 @@ venv_info() {
 }
 
 update_prompt() {
-  PS1="$(venv_info)%(!.%F{red}superuser.%F{green}%n) %F{cyan}%(5~|%-1~/…/%2~|%3~) $(parse_git_branch)%(!.%F{red}#.%F{magenta}$) %b%f%k"
+  PS1="$(venv_info)%(!.%F{red}superuser.%F{green}%n) %F{cyan}%(5~|%-1~/…/%2~|%3~) $(parse_git_branch)%(!.%F{red}#.%F{magenta}％) %b%f%k"
 }
 
 precmd_functions+=(update_prompt)
