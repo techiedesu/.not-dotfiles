e3c2f9799e6a0b065d49c8e7223d4152a2d2294a
Vlad Dyshakov <kawaii.nekotech@gmail.com>
2024-08-26 10:51:25 +0700

    Added aliases for equery

--- not-imports/not-aliases.zsh
+++ not-imports/not-aliases.zsh
@@ -52,6 +52,19 @@ if (($+commands[emerge])) then
   alias p="emerge"
   alias _p="_ p"
   alias emc="_ e /etc/portage/make.conf"
+  alias eqf='equery f'
+  alias equ='equery u'
+  alias eqh='equery h'
+  alias eqa='equery a'
+  alias eqb='equery b'
+  alias eql='equery l'
+  alias eqd='equery d'
+  alias eqg='equery g'
+  alias eqk='equery k'
+  alias eqm='equery m'
+  alias eqy='equery y'
+  alias eqs='equery s'
+  alias eqw='equery w'
 
   show-ebuild() {
     pager $(equery w "$1")
